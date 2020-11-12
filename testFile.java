cb53e66deac4ba432eeb7c81b8602e9b570064f4
RoloVoid <1225490622@qq.com>
2020-11-12 22:50:42 +0800

    实现了简单的二项频繁项集挖掘，理论上n项以上可以递推，不过还没实现

--- testFile.java
+++ testFile.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class testFile {
-    static double min_support=0.2;
+    static double min_support=0.8;
     //设置最小支持度阈值为0.2
 
     static double min_sup=2;
@@ -53,19 +53,29 @@ public class testFile {
         return result;
     }
     //构造子集的方法1:从频繁项子集中组合
-    
+
 
     public static HashMap<String[],Integer> searchFor(){
         return null;
     }
     //寻找频繁项的方法
 
+    public static String toString(LinkedList<String> a){
+        String b="";
+        for(int i=0;i<a.size()-1;i++){
+            b=b.concat(a.get(i)+",");
+        }
+        b=b.concat(a.get(a.size()-1)+"\t");
+        return b;
+    }
+    //写一个toString方法，便于最后写入文件
+
 
     public static void main(String[] args) throws Exception {
 
         File a = new File("C:\\Users\\86158\\Desktop\\ShoppingCart_dataset.csv");
         FileInputStream q = new FileInputStream(a);
-        FileOutputStream p=new FileOutputStream("C:\\Users\\86158\\Desktop\\result.txt");
+        FileOutputStream p=new FileOutputStream("C:\\Users\\86158\\Desktop\\result.csv");
 
         InputStreamReader m=new InputStreamReader(q);
         OutputStreamWriter n=new OutputStreamWriter(p);
@@ -79,17 +89,20 @@ public class testFile {
             String mn=test.readLine();
             if(mn!=null) {
                 LinkedList<String> temp=new LinkedList<>();
+
                 String[] recordTable=mn.split(",");
                 for(int i=1;i<recordTable.length;i++){
-                    temp.add(recordTable[i]);
+                    if(recordTable[i]!=""&&recordTable[i]!=" "&&recordTable[i].length()>1) {
+                        temp.add(recordTable[i]);
+                    }
                 }
+
                 database.add(temp);
             }
         }
         //导入数据到数据库
 
         q.close();
-        p.close();
 
         //从csv文件中读出数据，储存为linkedlist表，二维数组的首项为购买时间,已去除
 
@@ -103,14 +116,14 @@ public class testFile {
         //items项集
 
 
-        HashMap<String,Integer> freOneItem=new HashMap<>();
+        HashMap<String,Double> freOneItem=new HashMap<>();
         LinkedList<String> oneItemTable=new LinkedList<>();
 
         Iterator<String> itemsIter= items.iterator();
 
         while(itemsIter.hasNext()){
             String item1=itemsIter.next();
-            int counter=0;
+            double counter=0;
             for(LinkedList<String> record:database){
                 for(String item:record){
                     if (item.equals(item1)) {
@@ -125,6 +138,58 @@ public class testFile {
         }
         //获得频繁一项集
 
-        LinkedList<LinkedList<String>> freTwoItem=getSubset()
+        HashMap<LinkedList<String>,double[]> freTwoItems=new HashMap<>();
+        //从二项频繁项开始，字典里要同时有支持度和支持度计数
+        LinkedList<LinkedList<String>> twoItemsTable=new LinkedList<>();
+
+        LinkedList<LinkedList<String>> PfreTwoItem=getSubset(oneItemTable,2);
+        System.out.println(PfreTwoItem.get(5).get(1));
+        //获得可能的频繁二项集
+
+        LinkedList<LinkedList<String>> twoItem=new LinkedList<>();
+        for(LinkedList<String> op:database){
+            LinkedList<LinkedList<String>> ed=getSubset(op,2);
+            for(LinkedList<String> oe:ed){
+                twoItem.add(oe);
+            }
+        }
+        //获得所有二项集
+
+        for(LinkedList<String> pointer: PfreTwoItem){
+            int counter=0;
+            for(LinkedList<String> pointer2:twoItem){
+                if(pointer2.containsAll(pointer)){
+                    counter++;
+                }
+            }
+            if(counter>=min_sup){
+                for(String pointer3:pointer){
+                    double support=counter/(freOneItem.get(pointer3));
+                    if(support>min_support&&support<=1.0){
+                        LinkedList<String> out=new LinkedList<>();
+                        for(String z:pointer){
+                            out.add(z);
+                        }
+                        out.add(pointer3);
+                        double[] data=new double[]{counter,support};
+                        freTwoItems.put(out, data);
+                        test2.write(toString(out)+",");
+                        System.out.println(toString(out));
+                        test2.write(counter+"\t"+",");
+                        test2.write(String.valueOf(support)+"\n");
+                        test2.flush();
+                    }
+                }
+            }
+        }
+        try{
+            test2.close();
+        }catch (IOException e){
+            System.out.println("已经关闭");
+        }
+
+
+        //获得全部的频繁二项集，并输出
+
     }
 }
